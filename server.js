fa6159c7dec6bcb1c290583f78e95c630e14aa5b
Sham <shambhawi.kumari1393@gmail.com>
2020-08-13 22:42:30 -0700

    cleaned the code, removed unused function

--- server.js
+++ server.js
@@ -17,37 +17,9 @@ const fs = require('fs');
 //     labels.forEach(label => console.log(label.description));
 //   }
 
-// async function localizeObjects() {
-//   // Creates a client
-//   const client = new vision.ImageAnnotatorClient();
-//   //console.log(client);
-
-//   /**
-//    * TODO(developer): Uncomment the following line before running the sample.
-//    */
-//    const fileName = `./Image/test.jpg`;
-//   const request = {
-//     image: {content: fs.readFileSync(fileName)},
-//   };
-
-//   const [result] = await client.objectLocalization(request);
-//   const objects = result.localizedObjectAnnotations;
-//   objects.forEach(object => {
-//     console.log(`Name: ${object.name}`);
-//     console.log(`Confidence: ${object.score}`);
-//     const vertices = object.boundingPoly.normalizedVertices;
-//     vertices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
-//   });
-//   // [END vision_localize_objects]
-// }
-
-// localizeObjects();
-
 const client = new vision.ImageAnnotatorClient();
 async function quickstart() {
-  /**
-   * TODO(developer): Uncomment the following line before running the sample.
-   */
+  // pass image path here
   const gcsUri = `./Image/test3.jpg`;
 
   const [result] = await client.objectLocalization(gcsUri);
