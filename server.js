b7638577a620fa68a50fc179455aa9679c2e26a7
Sham <shambhawi.kumari1393@gmail.com>
2020-08-15 16:03:36 -0700

    image url

--- server.js
+++ server.js
@@ -17,10 +17,15 @@ const fs = require('fs');
 //     labels.forEach(label => console.log(label.description));
 //   }
 
+
+
+
 const client = new vision.ImageAnnotatorClient();
 async function quickstart() {
   // pass image path here
   const gcsUri = `./Image/test3.jpg`;
+
+  //const gcsUri = `https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbh3pPT5LZ--CNMCgz0vMJp4_d-yernWmHRA&usqp=CAU`;
 
   const [result] = await client.objectLocalization(gcsUri);
   const objects = result.localizedObjectAnnotations;
@@ -33,3 +38,56 @@ async function quickstart() {
 }
 
 quickstart();
+
+
+
+
+
+
+// Creates a client
+//const client = new vision.ImageAnnotatorClient();
+// async function imageFromUrl() {
+
+//   /**
+//    * TODO(developer): Uncomment the following line before running the sample.
+//    */
+//   const gcsUri = `https://cloud.google.com/vision/docs/images/bicycle_example.png`;
+
+//   const [result] = await client.objectLocalization(gcsUri);
+//   const objects = result.localizedObjectAnnotations;
+//   objects.forEach(object => {
+//     console.log(`Name: ${object.name}`);
+//     console.log(`Confidence: ${object.score}`);
+//     const veritices = object.boundingPoly.normalizedVertices;
+//     veritices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
+//   });
+// }
+
+// imageFromUrl();
+
+
+
+
+async function localizeObjectsGCS() {
+  // [START vision_localize_objects_gcs]
+  // Imports the Google Cloud client libraries
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  /**
+   * TODO(developer): Uncomment the following line before running the sample.
+   */
+   const gcsUri = `https://cloud.google.com/vision/docs/images/bicycle_example.png`;
+
+  const [result] = await client.objectLocalization(gcsUri);
+  const objects = result.localizedObjectAnnotations;
+  objects.forEach(object => {
+    console.log(`Name: ${object.name}`);
+    console.log(`Confidence: ${object.score}`);
+    const veritices = object.boundingPoly.normalizedVertices;
+    veritices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
+  });
+  // [END vision_localize_objects_gcs]
+}
+localizeObjectsGCS();
